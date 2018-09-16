920c7d1208219b26c53d1de072e315d5d720d073
shinigami-eyes <>
2018-09-16 12:20:54 +0200

    Import script

---
+++ import-data.js
@@ -0,0 +1,16 @@
+var bloomfilter = require("./extension/bloomfilter.js");
+var fs = require('fs');
+
+function importFile(txt, name) {
+    var b = new bloomfilter.BloomFilter(/*bits*/ 359440, 17);
+
+    for (var line of fs.readFileSync(txt).toString().split('\n')) {
+        line = line.trim();
+        if (line) b.add(line);
+    }
+    var bucketsAsBytes = new Uint8Array(b.buckets.buffer);
+    fs.writeFileSync("extension/data/" + name + ".dat", new Buffer(bucketsAsBytes));
+}
+
+importFile("C:\\ML\\facebook\\transphobic.txt", "transphobic");
+importFile("C:\\ML\\facebook\\t-friendly.txt", "t-friendly");
