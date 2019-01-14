419bd5928861a162aa2e6ef15e404a7b39c25e92
shinigami-eyes <43276258+shinigami-eyes@users.noreply.github.com>
2019-01-14 18:52:41 +0100

    Better build script

--- build.ps1
+++ build.ps1
@@ -1,6 +1,22 @@
-﻿node import-data.js
+﻿$ErrorActionPreference = 'Stop'
+
+$version = (gc .\extension\manifest.json | convertfrom-json).version
+$archiveDir = "C:\ML\shinigami-eyes-releases"
+
+
+$out = "$archiveDir\shinigami-eyes-$version.xpi"
+if(Test-Path $out){ throw 'Release already exists.' }
+
+node import-data.js
 cd extension
 del ..\ShinigamiEyes.xpi -ErrorAction Ignore
 7z a ..\ShinigamiEyes.xpi * -mm=Copy
 cd ..
+
+
+copy ShinigamiEyes.xpi $out
+7z a "$archiveDir\shinigami-eyes-lists-$version.7z" -mx9 C:\ML\facebook\transphobic.txt C:\ML\facebook\t-friendly.txt
+CheckExitCode
+
+
 
