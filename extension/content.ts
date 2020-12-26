194dd1fb5f661d2a01f694c0bc3194660ba12164
shinigami-eyes <43276258+shinigami-eyes@users.noreply.github.com>
2020-12-26 06:55:45 +0000

    Fix truncated Tumblr identifiers for some mentions

--- extension/content.ts
+++ extension/content.ts
@@ -429,7 +429,7 @@ function getIdentifierFromElementImpl(element: HTMLAnchorElement, originalTarget
         if (element.classList.contains('interlanguage-link-target')) return null;
     }
 
-    if (element.classList.contains('tumblelog')) return element.textContent.substr(1) + '.tumblr.com';
+    if (element.classList.contains('tumblelog')) return element.textContent.replace('@', '') + '.tumblr.com';
 
     const href = element.href;
     if (href && (!href.endsWith('#') || href.includes('&stick='))) return getIdentifierFromURLImpl(tryParseURL(href));
